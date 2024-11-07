3a906932b64313206e3ed6df0f363aaac718f7b0
fjarar <flavio.jara.rebolledo@gmail.com>
2024-11-07 17:22:23 -0300

    was changed the toString method and added more currency types to exchange

--- src/CurrencyExchange.java
+++ src/CurrencyExchange.java
@@ -7,8 +7,16 @@ import java.net.http.HttpResponse;
 
 public class CurrencyExchange {
     private String apiKey = "302ebc743d22a50aaf348f23";
-    public Currency currencyExchange(String currencyToBeExchanged, String finalCurrency, double amount){
-        URI url = URI.create("https://v6.exchangerate-api.com/v6/"+apiKey+"/pair/"+currencyToBeExchanged+"/"+finalCurrency+"/"+amount);
+    private String currencyToBeExchanged;
+    private String finalCurrency;
+    private double amount;
+    private double conversionResult;
+
+    public Currency currencyExchange(String currencyToBeExchanged, String finalCurrency, double amount) {
+        this.currencyToBeExchanged = currencyToBeExchanged;
+        this.finalCurrency = finalCurrency;
+        this.amount = amount;
+        URI url = URI.create("https://v6.exchangerate-api.com/v6/" + apiKey + "/pair/" + currencyToBeExchanged + "/" + finalCurrency + "/" + amount);
 
         HttpClient client = HttpClient.newHttpClient();
 
@@ -19,9 +27,19 @@ public class CurrencyExchange {
         try {
             HttpResponse<String> response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
-            return new Gson().fromJson(response.body(), Currency.class);
+            Currency currency = new Gson().fromJson(response.body(), Currency.class);
+            this.conversionResult = currency.conversion_result();
+            return currency;
         } catch (Exception e) {
             throw new RuntimeException("No se encontro la divisa ingresada");
         }
+    }
+
+    @Override
+    public String toString() {
+        return "La conversión de " + amount
+                + " [" + currencyToBeExchanged
+                + "] a [" + finalCurrency
+                + "] es igual a: "+conversionResult+" ["+finalCurrency+"]\n";
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -4,42 +4,69 @@ public class Principal {
     public static void main(String[] args) {
         Scanner userQuery = new Scanner(System.in);
         CurrencyExchange currencyExchange = new CurrencyExchange();
-        Currency currency;
         boolean continuar = true;
         do {
-            System.out.println("**************************************");
-            System.out.println("*Bienvenido/a al conversor de monedas*");
-            System.out.println("*****************MENU*****************");
-            System.out.println("**Seleccione una divisa a convertir***");
-            System.out.println("1.- Dólar(USD) a Peso Chileno(CLP)");
-            System.out.println("2.- Peso Chileno a Dólar");
-            System.out.println("9.- Salir");
-            System.out.println("Seleccione una opción valida:");
-            System.out.println("**************************************");
-            var opcion = Integer.valueOf(userQuery.nextLine());
-            var valueToConvert = 0.0;
-            switch (opcion){
-                case 1:
-                    System.out.println("Ingrese el monto a convertir");
-                    valueToConvert = Double.valueOf(userQuery.nextLine());
-                    currency = currencyExchange.currencyExchange("USD", "CLP", valueToConvert);
-                    System.out.println(currency);
-                    break;
-                case 2:
-                    System.out.println("Ingrese el monto a convertir");
-                    valueToConvert = Double.valueOf(userQuery.nextLine());
-                    currency = currencyExchange.currencyExchange("CLP", "USD", valueToConvert);
-                    System.out.println(currency);
-                    break;
-                case 9:
-                    System.out.println("Gracias por usar nuestro servicio");
-                    continuar=false;
-                    break;
-                default:
-                    System.out.println("La opción ingresada no es valida");
-                    break;
+            System.out.println("**********************************************");
+            System.out.println("**** Bienvenido/a al conversor de monedas ****");
+            System.out.println("******************* MENU *********************");
+            System.out.println("***** Seleccione una divisa a convertir ******");
+            System.out.println("* 1.- Dólar(USD) a Peso Chileno(CLP)         *");
+            System.out.println("* 2.- Peso Chileno a Dólar                   *");
+            System.out.println("* 3.- Euro(EUR) a Peso Chileno(CLP)          *");
+            System.out.println("* 4.- Peso Chileno a Euro(EUR)               *");
+            System.out.println("* 5.- Real Brasileño(BRL) a Peso Chileno(CLP)*");
+            System.out.println("* 6.- Peso Chileno a Real Brasileño(BRL)     *");
+            System.out.println("* 9.- Salir                                  *");
+            System.out.println("**      Seleccione una opción valida:        **");
+            System.out.println("**********************************************");
+            try {
+                var opcion = Integer.valueOf(userQuery.nextLine());
+                var valueToConvert = 0.0;
+                System.out.println("Ingrese el monto a convertir");
+                switch (opcion) {
+                    case 1:
+                        valueToConvert = Double.valueOf(userQuery.nextLine());
+                        currencyExchange.currencyExchange("USD", "CLP", valueToConvert);
+                        System.out.println(currencyExchange);
+                        break;
+                    case 2:
+                        valueToConvert = Double.valueOf(userQuery.nextLine());
+                        currencyExchange.currencyExchange("CLP", "USD", valueToConvert);
+                        System.out.println(currencyExchange);
+                        break;
+                    case 3:
+                        valueToConvert = Double.valueOf(userQuery.nextLine());
+                        currencyExchange.currencyExchange("EUR", "CLP", valueToConvert);
+                        System.out.println(currencyExchange);
+                        break;
+                    case 4:
+                        valueToConvert = Double.valueOf(userQuery.nextLine());
+                        currencyExchange.currencyExchange("CLP", "EUR", valueToConvert);
+                        System.out.println(currencyExchange);
+                        break;
+                    case 5:
+                        valueToConvert = Double.valueOf(userQuery.nextLine());
+                        currencyExchange.currencyExchange("BRL", "CLP", valueToConvert);
+                        System.out.println(currencyExchange);
+                        break;
+                    case 6:
+                        valueToConvert = Double.valueOf(userQuery.nextLine());
+                        currencyExchange.currencyExchange("CLP", "BRL", valueToConvert);
+                        System.out.println(currencyExchange);
+                        break;
+                    case 9:
+                        System.out.println("Gracias por usar nuestro servicio");
+                        continuar = false;
+                        break;
+                    default:
+                        System.out.println("La opción ingresada no es valida");
+                        break;
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada no válida. Por favor ingrese un número válido.");
             }
-        }while(continuar);
+        } while (continuar);
 
 
     }
