e4e96937643197bad6fb482b741756fad4504eb3
fjarar <flavio.jara.rebolledo@gmail.com>
2024-11-07 17:43:16 -0300

    adding a safety way to use the api key

--- src/CurrencyExchange.java
+++ src/CurrencyExchange.java
@@ -1,16 +1,29 @@
 import com.google.gson.Gson;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Properties;
 
 public class CurrencyExchange {
-    private String apiKey = "302ebc743d22a50aaf348f23";
+    private String apiKey;
     private String currencyToBeExchanged;
     private String finalCurrency;
     private double amount;
     private double conversionResult;
+
+    public CurrencyExchange() {
+        Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream("config.properties")) {
+            properties.load(input);
+            apiKey = properties.getProperty("api_key");
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load API key", e);
+        }
+    }
 
     public Currency currencyExchange(String currencyToBeExchanged, String finalCurrency, double amount) {
         this.currencyToBeExchanged = currencyToBeExchanged;
