2c1c5fda80fcac2837898797f32a2f75666babaa
fjarar <flavio.jara.rebolledo@gmail.com>
2024-11-07 12:43:32 -0300

    adding some API callings

---
+++ src/CurrencyExchange.java
@@ -0,0 +1,27 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class CurrencyExchange {
+    private String apiKey = "302ebc743d22a50aaf348f23";
+    public Currency currencyExchange(String currency){
+        URI url = URI.create("https://v6.exchangerate-api.com/v6/"+apiKey+"/latest/"+currency);
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Currency.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No se encontro la divisa ingresada");
+        }
+    }
+}
--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("test");
+        /*Scanner userQuery = new Scanner(System.in);
+        do {
+            System.out.println("***************************");
+            System.out.println("***********MENU************");
+            System.out.println("Seleccione una divisa a convertir");
+            System.out.println("***************************");
+        }while(true);*/
+
+        CurrencyExchange currencyExchange = new CurrencyExchange();
+        Currency currency = currencyExchange.currencyExchange("USD");
+        System.out.println(currency);
     }
 }
